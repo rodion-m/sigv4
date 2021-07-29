a041ab122c45bb4eb9c16036f7ec4c4046c45e8e
Rodion Mostovoy <36400912+rodion-m@users.noreply.github.com>
2021-07-29 03:57:58 +0300

    Stream<List<int>> replaced with List<int>

--- lib/src/client.dart
+++ lib/src/client.dart
@@ -82,11 +82,8 @@ class Sigv4Client implements BaseSigv4Client {
     if (path.isEmpty) {
       throw AssertionError('path is empty');
     }
-    if (body != null && !(body is String) && !(body is Stream<List<int>>)) {
+    if (body != null && !(body is String) && !(body is List<int>)) {
       throw AssertionError('body have to be String or List<int> or null');
-    }
-    if(body is Stream<List<int>>) {
-      body.listen((event) => body = event);
     }
 
     /// Split the URI into segments
@@ -121,7 +118,7 @@ class Sigv4Client implements BaseSigv4Client {
     }
 
     final hashedPayload = body is String
-        ? Sigv4.hashPayloadString(body as String)
+        ? Sigv4.hashPayloadString(body)
         : Sigv4.hashPayload(body as List<int>);
     if (signPayload) {
       headers[_x_amz_content_sha256] = hashedPayload;
